17b51f4e5f8f6562e240e96592522a4374665aec
jimmywei01 <min1990719@gmail.com>
2018-07-21 14:16:52 +0800

    Update 2018-07-21T06:16:41.304Z

--- src/js/all.js
+++
@@ -1,7 +0,0 @@
-// toggleClass 對被選元素進行添加/刪除類的切換操作
-$(document).ready(function () {
-    $('.showmenu').on('click', function (e) {
-        e.preventDefault();
-        $('body').toggleClass('menu-show');
-    });
-});
